205ff456be92fb26acc374d84773437ad7ee65c6
Brad Barnhill <bradfordbarnhill@gmail.com>
2024-01-14 14:55:43 -0600

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.6"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 
     namespace = "com.pnuema.bible.android"
