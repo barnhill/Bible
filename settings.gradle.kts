34465731e7385cd3c92887c516d6fa415a11ded9
Brad Barnhill <bradfordbarnhill@gmail.com>
2026-02-13 13:08:40 -0600

    Update Gradle dependencies and add Foojay resolver
    Updates various Gradle dependencies, including Compose BOM, Activity Compose, and the Android Gradle Plugin.
    
    Adds the Foojay resolver plugin to `settings.gradle.kts` and includes a new `gradle-daemon-jvm.properties` file to configure the JDK toolchain.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,6 +21,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.develocity") version "4.3.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
