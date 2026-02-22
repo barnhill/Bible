397d15bd487252fe0c32a89f34e5d10fff25263f
Brad Barnhill <bradfordbarnhill@gmail.com>
2026-02-22 15:35:08 -0600

    update dependencies and add padding to verses to make more readable

--- app/src/main/kotlin/com/pnuema/bible/android/ui/read/compose/VerseItem.kt
+++ app/src/main/kotlin/com/pnuema/bible/android/ui/read/compose/VerseItem.kt
@@ -1,13 +1,16 @@
 package com.pnuema.bible.android.ui.read.compose
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pnuema.bible.android.ui.BibleTheme
 import com.pnuema.bible.android.ui.read.state.VerseViewState
 
@@ -24,6 +27,9 @@ fun VerseItem(
         append(state.verseText.trimMargin("¶").trim())
     }
     Text(
+        modifier = Modifier.padding(
+            horizontal = 4.dp,
+        ),
         text = annotatedVerse,
         style = MaterialTheme.typography.bodyLarge,
         color = MaterialTheme.colorScheme.onBackground
